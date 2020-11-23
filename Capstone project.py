668f159f378a7cbac33acb25eeb1bc46f492327e
vignes81 <72426511+vignes81@users.noreply.github.com>
2020-11-23 11:42:18 +0800

    Add files via upload

---
+++ Capstone project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Mainly purposed for capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
